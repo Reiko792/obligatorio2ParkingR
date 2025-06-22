08a996c8e7ca5fdc19c69824f3347b0a9fd97be6
Nero-172 <reiko_9@outlook.com>
2025-06-22 00:38:16 -0300

    Prueba2

--- src/obligatorio2parking/VentanaReportes.java
+++ src/obligatorio2parking/VentanaReportes.java
@@ -12,10 +12,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author USUARIO
- */
+
 public class VentanaReportes extends javax.swing.JFrame {
 
     /**
